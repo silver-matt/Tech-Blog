434f2130823ca75bec3f654daafdc9ad0845e87a
Matt Silver <Mattsilver54@yahoo.com>
2022-06-26 20:27:16 -0700

    fixed errors

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,12 @@
 // required section
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const commentRoutes = require('./api/comment-routes');
 const postRoutes = require('./api/post-routes');
 const userRoutes = require('./api/user-routes')
 
 // use routes section
-router.use('./api', apiRoutes);
 router.use('/controllers/home-routes.js', homeRoutes);
 router.use('./api/comment-routes.js', commentRoutes);
 router.use('./api/post-routes.js', postRoutes);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const controller = require('./controllers').default;
+const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const session = require('express-session');
@@ -20,13 +20,13 @@ const sess = {
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(session(sess));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', controller);
+app.use(routes);
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
